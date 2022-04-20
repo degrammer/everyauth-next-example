9bd60edffa711c7b07089f7feecb649ea0848944
degrammer <ruben@fusebit.io>
2022-04-20 11:02:49 -0500

    initial commit

--- utils/encryption.js
+++ utils/encryption.js
@@ -24,7 +24,7 @@ const decrypt = (key, iv, tag, content) => {
   const alg = 'aes-256-gcm';
   const decipher = createDecipheriv(alg, Buffer.from(key, 'hex'), Buffer.from(iv, 'hex'));
   decipher.setAuthTag(Buffer.from(tag, 'hex'));
-  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  let decrypted = decipher.update(content, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
 
   return decrypted;

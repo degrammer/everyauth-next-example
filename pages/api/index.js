dfd5f39f0fa33c9d7039b8b0f5cc60f2d1b25927
degrammer <ruben@fusebit.io>
2022-04-19 23:20:23 -0500

    initial commit

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,6 +2,7 @@ const everyauth = require('@fusebit/everyauth-express');
 const { Octokit } = require('octokit');
 const crypto = require('crypto');
 const profile = require('./profile');
+const { join } = require('path');
 
 const app = require('express')();
 
@@ -18,6 +19,7 @@ const decryptedData = JSON.parse(decrypted);
 console.log('setting decrypted data', decryptedData);
 everyauth.config(decryptedData);
 
+app.set('views', join(__dirname, '..', '..','views'));
 app.engine("pug", require("pug").__express);
 app.set('view engine', 'pug');
 
@@ -37,9 +39,7 @@ app.get('/api/finished', async (req, res) => {
   const client = new Octokit({ auth: userCredentials?.accessToken });
   const { data } = await client.rest.users.getAuthenticated();
   const { data: repos } = await client.request('GET /user/repos', {});
-  res.status(200).json({ data, repos});
-/**
- 
+  //res.status(200).json({ data, repos});
 
   res.render('index', {
     title: `GitHub Profile for ${data.login}`,
@@ -47,8 +47,6 @@ app.get('/api/finished', async (req, res) => {
     used_storage: Math.round((data.disk_usage * 100) / data.plan.space, 2),
     public_repos: repos,
   });
-
-   */
 
 });
 

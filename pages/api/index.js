86f3f3a631c160369b42a269a9b64e68c4cd1048
degrammer <ruben@fusebit.io>
2022-04-19 22:58:19 -0500

    initial commit

--- pages/api/index.js
+++ pages/api/index.js
@@ -18,6 +18,7 @@ const decryptedData = JSON.parse(decrypted);
 console.log('setting decrypted data', decryptedData);
 everyauth.config(decryptedData);
 
+app.engine("pug", require("pug").__express);
 app.set('view engine', 'pug');
 
 app.use(
@@ -36,15 +37,14 @@ app.get('/api/finished', async (req, res) => {
   const client = new Octokit({ auth: userCredentials?.accessToken });
   const { data } = await client.rest.users.getAuthenticated();
   const { data: repos } = await client.request('GET /user/repos', {});
-  res.status(200).json({ data, repos});
-  /**
-   *   res.render('index', {
+  //res.status(200).json({ data, repos});
+
+  res.render('index', {
     title: `GitHub Profile for ${data.login}`,
     ...data,
     used_storage: Math.round((data.disk_usage * 100) / data.plan.space, 2),
     public_repos: repos,
   });
-   */
 
 });
 

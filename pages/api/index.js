8ec5ff32d2cdf7255c7149a108e27abfa586562b
degrammer <ruben@fusebit.io>
2022-04-19 20:55:13 -0500

    initial commit

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,15 +1,32 @@
+const everyauth = require('@fusebit/everyauth-express');
+const { Octokit } = require("octokit");
+
 const app = require('express')();
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${23213}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.set('view engine', 'pug');
+
+app.use(
+  '/api/githuboauth',
+  everyauth.authorize('githuboauth', {
+    finishedUrl: '/api/finished',
+    mapToUserId: (req) => 'degrammer', // req.user.id in production
+  })
+);
+
+app.get('/api/finished', async (req, res) => {
+  const userId = 'degrammer'  // req.user.id in production
+
+  // Send a message over slack.
+  const userCredentials = await everyauth.getIdentity('githuboauth', userId);
+  const client = new Octokit({ auth: userCredentials?.accessToken });
+  const { data } = await client.rest.users.getAuthenticated();
+  const { data: repos } = await client.request('GET /user/repos', {});
+  res.render('index',  { title: `GitHub Profile for ${data.login}`, ...data, used_storage: Math.round((data.disk_usage*100)/data.plan.space, 2) , public_repos: repos})
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.get('/api', (req, res) => {
+  res.redirect('/api/githuboauth');
 });
+
 
 module.exports = app;
